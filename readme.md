89c8ad25cc4cde3469dab8d79c6c0dd9dc1bb5d6
Igor Telmenko <igor-telmenko@yandex.ru>
2018-05-02 13:21:05 +0300

    First commit

---
+++ readme.md
@@ -0,0 +1,55 @@
+## Laravel Monolog MySQL Handler.
+
+This package will log errors into MySQL database instead storage/log/laravel.log file.
+
+### Installation
+
+~~~
+composer require itelmenko/laravel-mysql-logger
+~~~
+
+Open up `config/app.php` and find the `providers` key.
+
+~~~
+'providers' => array(
+    // ...
+    Logger\Laravel\Provider\MonologMysqlHandlerServiceProvider::class,
+);
+~~~
+
+Publish config using Laravel Artisan CLI.
+
+~~~
+php artisan vendor:publish
+~~~
+
+Migrate tables.
+
+~~~
+php artisan migrate
+~~~
+
+## Application Integration
+
+In your application `bootstrap/app.php` add:
+
+~~~php
+$app->configureMonologUsing(function($monolog) use($app) {
+    $monolog->pushHandler(new Logger\Monolog\Handler\MysqlHandler());
+});
+~~~
+
+## Environment configuration
+
+If you wish to change default table name to write the log into or database connection use following definitions in your .env file
+
+~~~
+DB_LOG_TABLE=logs
+DB_LOG_CONNECTION=mysql
+~~~
+
+## Credits
+
+Based on:
+
+- [N.M. Gilg4mesh] (https://raw.githubusercontent.com/Gilg4mesh/monolog-mysql/) 
---
+++ src/Logger/Monolog/Handler/MysqlHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Logger\Monolog\Handler;
+
+use DB;
+use Illuminate\Support\Facades\Auth;
+use Monolog\Logger;
+use Monolog\Handler\AbstractProcessingHandler;
+
+class MysqlHandler extends AbstractProcessingHandler
+{
+    protected $table;
+    protected $connection;
+
+    public function __construct($level = Logger::DEBUG, $bubble = true)
+    {
+        $this->table      = env('DB_LOG_TABLE', 'logs');
+        $this->connection = env('DB_LOG_CONNECTION', env('DB_CONNECTION', 'mysql'));
+
+        parent::__construct($level, $bubble);
+    }
+
+    protected function write(array $record)
+    {
+        $data = [
+            'instance'    => gethostname(),
+            'message'     => $record['message'],
+            'channel'     => $record['channel'],
+            'level'       => $record['level'],
+            'level_name'  => $record['level_name'],
+            'context'     => json_encode(array_map("utf8_encode", $record['context'] )),
+            'remote_addr' => isset($_SERVER['REMOTE_ADDR'])     ? ip2long($_SERVER['REMOTE_ADDR']) : null,
+            'user_agent'  => isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT']      : null,
+            'created_by'  => Auth::id() > 0 ? Auth::id() : null,
+            'created_at'  => $record['datetime']->format('Y-m-d H:i:s')
+        ];
+
+        DB::connection($this->connection)->table($this->table)->insert($data);
+    }
+} 
